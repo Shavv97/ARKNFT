81aec71f0fe02da84f4de5440c8c083c7de322f5
The Beast <johnnychavando5@gmail.com>
2022-02-15 12:03:45 -0800

    Hardhat test

--- contracts/ARKNFT.sol
+++ contracts/ARKNFT.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
 
-contract MyEpicNFT {
+contract ARKNFT {
     constructor() {
         console.log("This is my NFT contract. Whoa!");
     }
